59a6a650d6d55c8d8470d1e22c0534426d7f2291
Guillermocr221 <135352967+Guillermocr221@users.noreply.github.com>
2023-06-20 22:29:02 -0400

    Add files via upload

---
+++ APIMongoDBC#Prueba/APIMongoDBC#/Models/Automovil.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace APIMongoDBC_.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Automovil
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
+
+        [BsonElement("Nombre")]
+        public string Nombre { get; set; } = String.Empty;
+
+        [BsonElement("Año")]
+        public int Año { get; set; } 
+
+    }
+}
