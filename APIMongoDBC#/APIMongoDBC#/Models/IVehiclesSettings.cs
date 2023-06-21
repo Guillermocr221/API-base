11de11fa44178d6b0c3298cd712a82c98d64c27b
Guillermocr221 <135352967+Guillermocr221@users.noreply.github.com>
2023-06-20 21:08:52 -0500

    Delete APIMongoDBC# directory

--- APIMongoDBC#/APIMongoDBC#/Models/IVehiclesSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APIMongoDBC_.Models
-{
-    public interface IVehiclesSettings
-    {
-        public string Collection { get; set; }
-        public string Database { get; set; }
-        public string Connection { get; set; }
-    }
-}
--- APIMongoDBC#/APIMongoDBC#/Models/VehiclesSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APIMongoDBC_.Models
-{
-    public class VehiclesSettings : IVehiclesSettings
-    {
-        public string Collection { get ; set; } = String.Empty;
-        public string Database { get ; set ; } = String.Empty;
-        public string Connection { get; set; } = String.Empty;
-    }
-}
